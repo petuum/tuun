9e3d78e2dda9138a59f1051b10c3d2d81247c336
willieneis <willie.neiswanger@gmail.com>
2020-06-24 04:36:03 -0400

    examples: add minimal 2d quick_start script

---
+++ examples/quick_start/01_minimal_example_2d.py
@@ -0,0 +1,23 @@
+from tuumbo import Tuumbo, RealDomain, AcqOptimizer, SimpleGp
+import numpy as np
+
+# define initial dataset
+data = {'x': [np.array([0., 0.]), np.array([0., 1.]), np.array([1., 0.]),
+              np.array([1., 1.]), np.array([.5, .5])],
+        'y': [6., 3., 4., 5., 2.]}
+
+# define model
+model = SimpleGp({'ls': 3.7, 'alpha': 1.85, 'sigma': 1e-5})
+
+# define acqfunction
+acqfunction = {'acq_str': 'ei', 'n_gen': 100}
+
+# define acqoptimizer
+acqoptimizer = AcqOptimizer(domain={'min_max': [(-5, 5), (-5, 5)]})
+
+# define tuumbo
+tu = Tuumbo(data, model, acqfunction, acqoptimizer)
+
+# get acquisition optima
+acq_optima = tu.get()
+print('acq_optima: {}'.format(acq_optima))
