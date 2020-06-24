c08c9210ca077c3df74e5fe0d04b130b978128da
willieneis <willie.neiswanger@gmail.com>
2020-06-24 04:17:05 -0400

    add tuumbo/acq/, AcqFunction, AcqOptimizer, Acquisitioner

--- tuumbo/acq/__init__.py
+++ tuumbo/acq/__init__.py
@@ -2,11 +2,5 @@
 Code for acquisition functions and acquisition optimization.
 """
 
-def get_default_acqfunction(verbose=True):
-    """Return default acquisition function."""
-    return None
-
-
-def get_default_acqoptimizer(data, verbose=True):
-    """Return default acquisition optimizer."""
-    return None
+from .acqfun import AcqFunction
+from .acqopt import AcqOptimizer
