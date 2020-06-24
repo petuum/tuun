9e656ce220736277c28d2d6d781a791e4f7372ee
willieneis <willie.neiswanger@gmail.com>
2020-06-24 01:36:23 -0400

    add acq/

---
+++ tuumbo/acq/__init__.py
@@ -0,0 +1,12 @@
+"""
+Code for acquisition functions and acquisition optimization.
+"""
+
+def get_default_acqfunction(verbose=True):
+    """Return default acquisition function."""
+    return None
+
+
+def get_default_acqoptimizer(data, verbose=True):
+    """Return default acquisition optimizer."""
+    return None
