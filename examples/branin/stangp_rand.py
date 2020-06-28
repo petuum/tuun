ae6e8dfb1bc62506267f036b95c02242206fcb9a
willieneis <willie.neiswanger@gmail.com>
2020-06-28 04:22:15 -0400

    examples/branin: add script for stangp with rand acqoptimizer

---
+++ examples/branin/stangp_rand.py
@@ -0,0 +1,37 @@
+from tuumbo import Tuumbo, RealDomain, AcqOptimizer, StanGp
+from examples.branin.branin import branin, get_branin_domain
+
+# define dataset
+data = {'x': [], 'y': []}
+
+# define model
+model = StanGp({'ndimx':2, 'model_str':'optfixedsig', 'ig1':4., 'ig2':3.,
+                'n1':1., 'n2':1., 'sigma':1e-5, 'niter':70})
+
+# define acqfunction
+acqfunction = {'acq_str': 'ei', 'n_gen': 500}
+
+# define acqoptimizer
+acqoptimizer = AcqOptimizer(domain=get_branin_domain())
+
+# define tuumbo
+tu = Tuumbo(data, model, acqfunction, acqoptimizer, seed=11)
+
+# define function
+f = branin
+
+# BO loop
+for i in range(50):
+
+    # Choose next x and query f(x)
+    x = tu.get()
+    y = f(x)
+
+    # Update data and reset data in tu
+    data['x'].append(x)
+    data['y'].append(y)
+    tu.set_data(data)
+
+    # Print iter info
+    bsf = min(data['y'])
+    print('i: {},    x: {},\ty: {:.4f},\tBSF: {:.4f}'.format(i, x, y, bsf))
