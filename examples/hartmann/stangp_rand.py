3c987b13567d289044b557aa8c8edb3626e2cc77
willieneis <willie.neiswanger@gmail.com>
2020-07-02 01:32:46 -0400

    add examples/hartmann/ scripts

---
+++ examples/hartmann/stangp_rand.py
@@ -0,0 +1,47 @@
+from tuun import Tuun, StanGp, AcqOptimizer
+from examples.hartmann.hartmann import hartmann6, get_hartmann6_domain
+
+# define dataset
+data = {'x': [], 'y': []}
+
+# define model
+model = StanGp(
+    {
+        'ndimx': 6,
+        'model_str': 'optfixedsig',
+        'ig1': 4.0,
+        'ig2': 3.0,
+        'n1': 1.0,
+        'n2': 1.0,
+        'sigma': 1e-5,
+        'niter': 70,
+    }
+)
+
+# define acqfunction
+acqfunction = {'acq_str': 'ei', 'n_gen': 500}
+
+# define acqoptimizer
+acqoptimizer = AcqOptimizer(domain=get_hartmann6_domain())
+
+# define tuun
+tu = Tuun(data, model, acqfunction, acqoptimizer, seed=11)
+
+# define function
+f = hartmann6
+
+# BO loop
+for i in range(50):
+
+    # Choose next x and query f(x)
+    x = tu.get()
+    y = f(x)
+
+    # Update data and reset data in tu
+    data['x'].append(x)
+    data['y'].append(y)
+    tu.set_data(data)
+
+    # Print iter info
+    bsf = min(data['y'])
+    print('i: {},    x: {},\ty: {:.4f},\tBSF: {:.4f}'.format(i, x, y, bsf))
