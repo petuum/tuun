81405162faf3a7604ee1a75328d3b41038d51666
willieneis <willie.neiswanger@gmail.com>
2020-07-02 03:44:58 -0400

    examples/branin: update scripts to use SimpleBo wrapper

--- examples/branin/stangp_cobyla.py
+++ examples/branin/stangp_cobyla.py
@@ -1,4 +1,4 @@
-from tuun import Tuun, StanGp, CobylaAcqOptimizer
+from tuun import Tuun, StanGp, CobylaAcqOptimizer, SimpleBo
 from examples.branin.branin import branin, get_branin_domain
 
 # define dataset
@@ -30,18 +30,6 @@ tu = Tuun(data, model, acqfunction, acqoptimizer, seed=11)
 # define function
 f = branin
 
-# BO loop
-for i in range(50):
-
-    # Choose next x and query f(x)
-    x = tu.get()
-    y = f(x)
-
-    # Update data and reset data in tu
-    data['x'].append(x)
-    data['y'].append(y)
-    tu.set_data(data)
-
-    # Print iter info
-    bsf = min(data['y'])
-    print('i: {},    x: {},\ty: {:.4f},\tBSF: {:.4f}'.format(i, x, y, bsf))
+# define and run BO
+bo = SimpleBo(tu, f, params={'n_iter': 50})
+results = bo.run()
