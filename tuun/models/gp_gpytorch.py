b3ee03ce2da460e602f36293111750c4446b551c
willieneis <willie.neiswanger@gmail.com>
2020-08-05 17:15:54 -0700

    examples/branin: add scripts for GpytorchGp with rand and cobyla

--- tuun/models/gp_gpytorch.py
+++ tuun/models/gp_gpytorch.py
@@ -13,20 +13,6 @@ from ..util.data_transform import DataTransformer
 from .gp.gp_utils import sample_mvn
 
 
-class ExactGPModel(gpytorch.models.ExactGP):
-    """GPyTorch ExactGpModel class."""
-
-    def __init__(self, train_x, train_y, likelihood):
-        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
-
-    def forward(self, x):
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
-
-
 class GpytorchGp:
     """
     Gaussian processes implemented with GPyTorch.
@@ -164,3 +150,17 @@ class GpytorchGp:
     def print_str(self):
         """Print a description string."""
         print('*GpytorchGp with params={}'.format(self.params))
+
+
+class ExactGPModel(gpytorch.models.ExactGP):
+    """GPyTorch ExactGpModel class."""
+
+    def __init__(self, train_x, train_y, likelihood):
+        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
