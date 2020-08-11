f7342f3b276b15bcd2d6be46a9c84accef40750e
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-08-11 02:52:48 -0400

    examples/quick_start: add scripts using ProboBackend

---
+++ examples/quick_start/03b_probo_backend_suggest.py
@@ -0,0 +1,14 @@
+from tuun.backend import ProboBackend
+
+model_config={'name': 'gpytorchgp'}
+acqfunction_config = {'name': 'default', 'acq_str': 'ucb', 'n_gen': 500}
+acqoptimizer_config = {'name': 'neldermead', 'max_iter': 200, 'jitter': True}
+domain_config = {'min_max': [(-8.0, 7.0), (-7.0, 8.0)]}
+
+data = {
+    'x': [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0]],
+    'y': [6.0, 3.0, 4.0],
+}
+
+pb = ProboBackend(model_config, acqfunction_config, acqoptimizer_config, domain_config)
+suggestion = pb.suggest(data)
---
+++ examples/quick_start/04_probo_backend_tune.py
@@ -0,0 +1,11 @@
+from tuun.backend import ProboBackend
+
+model_config={'name': 'simplegp', 'ls': 3.0, 'alpha': 1.5, 'sigma': 1e-5}
+acqfunction_config = {'name': 'default', 'acq_str': 'ei'}
+acqoptimizer_config = {'name': 'default', 'max_iter': 200}
+domain_config = {'min_max': [(-5, 5)]}
+
+f = lambda x: x ** 4 - x ** 2 + 0.1 * x
+
+pb = ProboBackend(model_config, acqfunction_config, acqoptimizer_config, domain_config)
+results = pb.tune_function(f, 20)
