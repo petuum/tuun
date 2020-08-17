bae9864c2fba7ddf6a0b7cd2d4de6d91667b6bc2
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-08-17 18:54:03 -0400

    Tuun class now working

--- tuun/main.py
+++ tuun/main.py
@@ -1,6 +1,7 @@
 """
 Main interface for Tuun.
 """
+from argparse import Namespace
 
 from .backend import ProboBackend, DragonflyBackend
 
@@ -25,7 +26,9 @@ class Tuun:
 
         self.config = Namespace()
 
-        # Backend
+        # Tuun parameters
+        self.config.seed = getattr(config, 'seed', None)
+
         assert config.backend in ['probo', 'dragonfly']
         self.config.backend = getattr(config, 'backend', 'probo')
 
@@ -64,7 +67,7 @@ class Tuun:
             opt_config = getattr(config, 'opt_config', None)
             if opt_config is None:
                 opt_config = {'name': domain_config['name']}
-            self.config.model_config = model_config
+            self.config.opt_config = opt_config
 
             dragonfly_config = getattr(config, 'dragonfly_config', None)
             self.config.dragonfly_config = dragonfly_config
@@ -72,11 +75,19 @@ class Tuun:
     def _set_backend(self):
         """Set Tuun backend tuning system."""
         if self.config.backend == 'probo':
-            pass
-            #self.backend = ProboBackend()
+            self.backend = ProboBackend(
+                model_config=self.config.model_config,
+                acqfunction_config=self.config.acqfunction_config,
+                acqoptimizer_config=self.config.acqoptimizer_config,
+                domain_config=self.config.domain_config,
+                probo_config=self.config.probo_config,
+            )
         elif self.config.bakend == 'dragonfly':
-            pass
-            #self.backend = DragonflyBackend()
+            self.backend = DragonflyBackend(
+                domain_config=self.config.domain_config,
+                opt_config=self.config.opt_config,
+                dragonfly_config=self.config.dragonfly_config,
+            )
 
     def suggest_to_minimize(self, data=None, verbose=True, seed=None):
         """
@@ -91,7 +102,13 @@ class Tuun:
         seed : int
             If not None, set the random seed to seed.
         """
-        pass
+        if seed is None:
+            seed = self.config.seed
+
+        suggestion = self.backend.suggest_to_minimize(
+            data=data, verbose=verbose, seed=seed
+        )
+        return suggestion
 
     def minimize_function(
         self, f, n_iter=10, data=None, data_update_fun=None, verbose=True, seed=None
@@ -114,4 +131,28 @@ class Tuun:
         seed : int
             If not None, set the random seed to seed.
         """
-        pass
+        if seed is None:
+            seed = self.config.seed
+
+        if self.config.backend == 'dragonfly':
+            if data is not None:
+                #  TODO: print warning
+                pass
+
+            if data_update_fun is not None:
+                #  TODO: print warning
+                pass
+
+            result = self.backend.minimize_function(
+                f=f, n_iter=n_iter, verbose=verbose, seed=seed
+            )
+
+        elif self.config.backend == 'probo':
+            result = self.backend.minimize_function(
+                f=f,
+                n_iter=n_iter,
+                data=data,
+                data_update_fun=data_update_fun,
+                verbose=verbose,
+                seed=seed,
+            )
