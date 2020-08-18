8584d3694126a069a078e691110c6de8d318125d
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-08-17 21:04:49 -0400

    examples/quick_start: fix DragonflyBackend suggest real examples

--- examples/quick_start/05_dragonfly_backend_suggest.py
+++ examples/quick_start/05_dragonfly_backend_suggest.py
@@ -2,11 +2,12 @@ from tuun.backend import DragonflyBackend
 
 domain_config = {'name': 'real', 'bounds_list': [[-5, 5]]}
 opt_config = {'name': 'real'}
+dragonfly_config = {'acq_str': 'ucb-ei', 'n_init_rs': 0}
 
 data = {
     'x': [0.0, 1.0, 2.0],
     'y': [6.0, 3.0, 4.0],
 }
 
-db = DragonflyBackend(domain_config, opt_config)
+db = DragonflyBackend(domain_config, opt_config, dragonfly_config)
 suggestion = db.suggest_to_minimize(data)
--- examples/quick_start/05b_dragonfly_backend_suggest.py
+++ examples/quick_start/05b_dragonfly_backend_suggest.py
@@ -2,11 +2,12 @@ from tuun.backend import DragonflyBackend
 
 domain_config = {'name': 'real', 'bounds_list': [(-8.0, 7.0), (-7.0, 8.0)]}
 opt_config = {'name': 'real'}
+dragonfly_config = {'acq_str': 'ucb-ei', 'n_init_rs': 0}
 
 data = {
     'x': [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0]],
     'y': [6.0, 3.0, 4.0],
 }
 
-db = DragonflyBackend(domain_config, opt_config)
+db = DragonflyBackend(domain_config, opt_config, dragonfly_config)
 suggestion = db.suggest_to_minimize(data)
