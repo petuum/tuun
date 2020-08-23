05c407e929ccb87d39fcb177d897b230a00d10d0
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-08-23 04:52:07 -0400

    add examples/nni_simple_2d with 2d real domain

---
+++ examples/nni_simple_2d/run_nni.sh
@@ -0,0 +1,1 @@
+nnictl create --config examples/nni_simple_2d/config.yml
---
+++ examples/nni_simple_2d/trial.py
@@ -0,0 +1,19 @@
+import numpy as np
+import nni
+
+f_s = lambda x: x ** 4 - x ** 2 + 0.1 * x
+f = lambda x_list: f_s(x_list[0]) + f_s(x_list[1])
+
+x = nni.get_next_parameter()
+print('x = {}'.format(x))
+
+#xin = x['suggestion']['__ndarray__'][0]
+xin = x['suggestion']['__ndarray__']
+print('xin = {}'.format(xin))
+
+print('querying function now')
+y = f(xin)
+print('y = {}'.format(y))
+print('-----')
+
+nni.report_final_result(y)
