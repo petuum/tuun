3bc4c48238c3bdb4ceb7c9e2bb1a352dec97f91a
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-09-04 22:52:41 -0400

    examples/nni_simple_product: update trial function definition

--- examples/nni_simple_product/trial.py
+++ examples/nni_simple_product/trial.py
@@ -1,15 +1,13 @@
 import numpy as np
 import nni
 
-f_s = lambda x: x ** 4 - x ** 2 + 0.1 * x
+f_s = lambda x: x[0] ** 4 - x[0] ** 2 + 0.1 * x[0]
 f = lambda x_list: f_s(x_list[0]) + f_s(x_list[1])
 
 x = nni.get_next_parameter()
 print('x = {}'.format(x))
 
-xin1 = x['suggestion'][0]['__ndarray__'][0]
-xin2 = x['suggestion'][1]['__ndarray__'][0]
-xin = [xin1, xin2]
+xin = x['suggestion']
 print('xin = {}'.format(xin))
 
 print('querying function now')
