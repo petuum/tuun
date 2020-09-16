52d9e9cb0866c41fcfe3f98ede2764cfce9a324f
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-09-16 04:31:10 -0400

    examples/quick_start: update list-real script to use list of str

--- examples/quick_start/12e_tuun_minimize.py
+++ examples/quick_start/12e_tuun_minimize.py
@@ -14,7 +14,7 @@ config = {
         'name': 'product',
         'n_iter_bcd': 3,
         #'rand_every': 4,
-        'n_init_rs': 10,
+        'n_init_rs': 3,
         'pao_config_list': [
             {'name': 'default'},
             {'name': 'cobyla', 'init_str': 'init_opt'},
@@ -23,13 +23,16 @@ config = {
     'domain_config': {
         'name': 'product',
         'dom_config_list': [
-            {'name': 'list', 'domain_list': [[4], [5], [0], [0.7], [-0.7]]},
+            {'name': 'list', 'domain_list': ['a', 'b', 'c', 'd', 'e']},
             {'name': 'real', 'min_max': [(-10, 10)]},
         ],
     },
 }
 tu = Tuun(config)
 
+# Define function on list-real domain points
+str_map = {'a': [4], 'b': [5], 'c': [0], 'd': [0.7], 'e': [-0.7]}
 f_s = lambda x: x[0] ** 4 - x[0] ** 2 + 0.1 * x[0]
-f = lambda x_list: f_s(x_list[0]) + f_s(x_list[1])
+f = lambda x_list: f_s(str_map[x_list[0]]) + f_s(x_list[1])
+
 result = tu.minimize_function(f, 50)
