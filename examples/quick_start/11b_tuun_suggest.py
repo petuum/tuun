2aca3fee66c3c4b0f777c857df33a77ba0de1334
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-10-08 05:44:56 -0400

    examples/quick_start: update DragonflyBackend config

--- examples/quick_start/11b_tuun_suggest.py
+++ examples/quick_start/11b_tuun_suggest.py
@@ -3,7 +3,7 @@ from tuun.main import Tuun
 config = {
     'backend': 'dragonfly',
     'seed': 11,
-    'domain_config': {'name': 'real', 'bounds_list': [(-0.5, 2.5), (-0.6, 2.6)]},
+    'domain_config': {'name': 'real', 'min_max': [[-0.5, 2.5], [-0.6, 2.6]]},
     'opt_config': {'name': 'real'},
     'dragonfly_config': {'acq_str': 'ucb-ei', 'n_init_rs': 0}
 }
