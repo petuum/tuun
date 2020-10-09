94f066bd5203ba4132451d64e419614413594094
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-10-09 14:00:16 -0400

    add examples/nni_simple_nodomain without domain in tuun_config

---
+++ examples/nni_simple_nodomain/trial.py
@@ -0,0 +1,17 @@
+import numpy as np
+import nni
+
+f = lambda x: x[0] ** 4 - x[0] ** 2 + 0.1 * x[0]
+
+x = nni.get_next_parameter()
+print('x = {}'.format(x))
+
+xin = x['suggestion']
+print('xin = {}'.format(xin))
+
+print('querying function now')
+y = f(xin)
+print('y = {}'.format(y))
+print('-----')
+
+nni.report_final_result(y)
