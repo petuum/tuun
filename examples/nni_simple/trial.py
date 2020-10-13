1b9d95f4a4e6cd8827da13b2562e94e5838d0f00
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-10-13 05:42:33 -0400

    examples/nni_simple: update function definition

--- examples/nni_simple/trial.py
+++ examples/nni_simple/trial.py
@@ -1,7 +1,8 @@
 import numpy as np
 import nni
 
-f = lambda x: x[0] ** 4 - x[0] ** 2 + 0.1 * x[0]
+f_s = lambda x: x[0] ** 4 - x[0] ** 2 + 0.1 * x[0]
+f = lambda x_list: f_s(x_list[0])
 
 x = nni.get_next_parameter()
 print('x = {}'.format(x))
