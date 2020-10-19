400a32afcf2d22a0aac61f68b96312b95c615066
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-10-18 21:00:27 -0400

    examples/nni_simple_2d: fix bug in function definition

--- examples/nni_simple_2d/trial.py
+++ examples/nni_simple_2d/trial.py
@@ -2,7 +2,7 @@ import numpy as np
 import nni
 
 f_s = lambda x: x ** 4 - x ** 2 + 0.1 * x
-f = lambda x_list: f_s(x_list[0]) + f_s(x_list[1])
+f = lambda x_list: f_s(x_list[0][0]) + f_s(x_list[0][1])
 
 x = nni.get_next_parameter()
 print('x = {}'.format(x))
