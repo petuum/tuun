845de7d5dc1f60dc5806e357d0f37b6e8b3159fc
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-10-27 05:11:25 -0400

    add examples/hartmann/ example scripts

---
+++ examples/hartmann/00_stan.py
@@ -0,0 +1,17 @@
+from tuun.main import Tuun
+from hartmann import hartmann6
+
+config = {
+    'seed': 11,
+    'acqfunction_config': {'name': 'default', 'acq_str': 'ei', 'n_gen': 500},
+    'model_config': {'name': 'standistmatgp'},
+    'normalize_real': True,
+}
+tu = Tuun(config)
+
+search_space_list = [('real', [[0, 1]] * 6)]
+tu.set_config_from_list(search_space_list)
+
+f = hartmann6
+
+result = tu.minimize_function(f, 50)
