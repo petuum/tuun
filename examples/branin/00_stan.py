e9d06309a9e32f293925b328209dc340a627518d
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-10-27 05:26:13 -0400

    examples/branin: remove old files, add new Tuun example scripts

---
+++ examples/branin/00_stan.py
@@ -0,0 +1,18 @@
+from tuun.main import Tuun
+from examples.branin.branin import branin
+
+config = {
+    'seed': 12,
+    'acqfunction_config': {'name': 'default', 'acq_str': 'ei', 'n_gen': 500},
+    'model_config': {'name': 'standistmatgp'},
+    'normalize_real': True,
+}
+tu = Tuun(config)
+
+search_space_list = [('real', [[-5, 10], [0, 15]])]
+#search_space_list = [('real', [[-5, 10]]), ('real', [[0, 15]])]
+tu.set_config_from_list(search_space_list)
+
+f = branin
+
+result = tu.minimize_function(f, 30)
