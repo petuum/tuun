d03de6e62c610218a43d9a2f8fbc4ccb6514708c
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-10-28 02:37:13 -0400

    examples/branin40: fix function def and update config

--- examples/branin40/00_stan.py
+++ examples/branin40/00_stan.py
@@ -4,8 +4,8 @@ from examples.branin.branin import branin
 
 config = {
     'seed': 11,
-    'acqfunction_config': {'name': 'default', 'acq_str': 'ucb', 'n_gen': 500},
-    'acqopt_config': {'name': 'neldermead'}, # TODO: set n_init_rs
+    'acqfunction_config': {'name': 'default', 'acq_str': 'ei', 'n_gen': 500},
+    'acqoptimizer_config': {'name': 'neldermead', 'n_init_rs': 10},
     'model_config': {'name': 'standistmatgp'},
 }
 tu = Tuun(config)
@@ -13,6 +13,6 @@ tu = Tuun(config)
 search_space_list = [('real', [[-5, 10], [0, 15]]  * 20)]
 tu.set_config_from_list(search_space_list)
 
-f = lambda x: np.sum([branin(x[0][2 * i : 2 * i + 2]) for i in range(20)])
+f = lambda x: np.sum([branin(x[2 * i : 2 * i + 2]) for i in range(20)])
 
 result = tu.minimize_function(f, 120)
