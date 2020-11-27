f2e6c16340bb1eaf8a88928974952e6a4d831ff2
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-11-27 06:48:05 -0500

    examples/nni_simple: update function def in trial

--- examples/nni_simple/trial.py
+++ examples/nni_simple/trial.py
@@ -1,8 +1,7 @@
 import numpy as np
 import nni
 
-f_s = lambda x: x[0] ** 4 - x[0] ** 2 + 0.1 * x[0]
-f = lambda x_list: f_s(x_list[0])
+f = lambda x: x[0] ** 4 - x[0] ** 2 + 0.1 * x[0]
 
 x = nni.get_next_parameter()
 print('x = {}'.format(x))
