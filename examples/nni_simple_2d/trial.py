f60a1625db98a6b5403c9a85bf6f8529dd36930a
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-11-27 06:49:29 -0500

    examples/nni_simple_2d: update config details and function def in trial

--- examples/nni_simple_2d/trial.py
+++ examples/nni_simple_2d/trial.py
@@ -2,13 +2,11 @@ import numpy as np
 import nni
 
 f_s = lambda x: x ** 4 - x ** 2 + 0.1 * x
-f = lambda x_list: f_s(x_list[0][0]) + f_s(x_list[0][1])
+f = lambda x_list: f_s(x_list[0]) + f_s(x_list[1])
 
 x = nni.get_next_parameter()
 print('x = {}'.format(x))
 
-#xin = x['suggestion']['__ndarray__'][0]
-#xin = x['suggestion']['__ndarray__']
 xin = list(x.values())
 print('xin = {}'.format(xin))
 
