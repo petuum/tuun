bff2d1f26678036bdba5025ebd280c5e4c7a980b
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-11-29 04:21:40 -0500

    add examples/nni_simple_mixed

---
+++ examples/nni_simple_mixed/run_nni.sh
@@ -0,0 +1,1 @@
+nnictl create --config examples/nni_simple_mixed/config.yml
---
+++ examples/nni_simple_mixed/trial.py
@@ -0,0 +1,20 @@
+import numpy as np
+import nni
+
+
+# Proxy function 
+str_map = {'a': 4, 'b': 5, 'c': 0, 'd': 0.7, 'e': -0.7}
+f_s = lambda x: x ** 4 - x ** 2 + 0.1 * x
+f = lambda x_list: f_s(str_map[x_list[0]]) + f_s(x_list[1])
+
+x = nni.get_next_parameter()
+print('x = {}'.format(x))
+
+xin = list(x.values())
+print('xin = {}'.format(xin))
+
+print('Querying function now')
+y = f(xin)
+print('y = {}'.format(y))
+
+nni.report_final_result(y)
